2f06dd481c7ce964e40bacd614588ece807412b2
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-11-06 19:49:31 +0700

    feat: made unauthenticated landing page

--- users/views.py
+++ users/views.py
@@ -3,8 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def show_profile(request):
+def show_landing(request):
+    return render(request, "show_landing.html")
 
+
+def show_profile(request):
     context = {"name": "John Doe", "email": "johndoe@email.com"}
 
     return render(request, "show_profile.html", context)
+
+
+def show_login(request):
+    context = {"name": "John Doe", "email": "johndoe@email.com"}
+
+    return render(request, "show_login.html", context)
+
+
+def show_register(request):
+    context = {"name": "John Doe", "email": "johndoe@email.com"}
+
+    return render(request, "show_register.html", context)
