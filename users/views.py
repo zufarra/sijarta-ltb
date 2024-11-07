c7992a209effe1de6a8b1c33ea4ab0b110d782f2
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-11-07 22:38:51 +0700

    feat: create profile page

--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ def show_profile(request):
         "user": {
             "is_authenticated": True,
             "name": "John Doe",
-            "is_pengguna": True,
+            "is_pengguna": False,
             "email": "johndoe@mail.com",
             "phone": "123-456-7890",
             "address": "1234 Elm St",
