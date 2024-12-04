de3c97b089356654a68d98c43d59d5f9b575d56f
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-12-04 20:44:40 +0700

    migrate: add trigger rafli

---
+++ db/migrations/0004_add_trigger_selesai_pesanan.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION selesai_pesanan()
+RETURNS TRIGGER AS $$
+DECLARE pelanggan UUID;
+DECLARE pekerja UUID;
+DECLARE biaya DECIMAL;
+BEGIN
+    SELECT PJ.id_pelanggan INTO pelanggan FROM TR_PEMESANAN_STATUS PS
+    JOIN TR_PEMESANAN_JASA PJ ON (PS.id_tr_pemesanan = PJ.id) 
+    WHERE PS.id_tr_pemesanan = NEW.id_tr_pemesanan;
+
+    SELECT PJ.id_pekerja INTO pekerja FROM TR_PEMESANAN_STATUS PS
+    JOIN TR_PEMESANAN_JASA PJ ON (PS.id_tr_pemesanan = PJ.id) 
+    WHERE PS.id_tr_pemesanan = NEW.id_tr_pemesanan;
+
+    SELECT PJ.total_biaya INTO biaya FROM TR_PEMESANAN_STATUS PS
+    JOIN TR_PEMESANAN_JASA PJ ON (PS.id_tr_pemesanan = PJ.id) 
+    WHERE PS.id_tr_pemesanan = NEW.id_tr_pemesanan;
+
+    IF NEW.Id_status = 'f33e4567-e89b-12d3-a456-426614174004' THEN
+        UPDATE SIJARTA.USER SET saldo_mypay = saldo_mypay + biaya
+        WHERE id = pekerja;
+
+        INSERT INTO TR_MYPAY VALUES
+        (gen_random_uuid(), pekerja, NOW(), biaya, 'f23e4567-e89b-12d3-a456-426614174005');
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_selesai_pesanan
+AFTER UPDATE ON TR_PEMESANAN_STATUS
+FOR EACH ROW
+EXECUTE FUNCTION selesai_pesanan(); 
