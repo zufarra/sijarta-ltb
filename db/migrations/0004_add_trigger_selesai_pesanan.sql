a478260a693ddf070536b85615a2bd57c52a3a71
rafliesa <muhammadrafli1579@gmail.com>
2024-12-05 14:02:35 +0700

    fix: fix trigger rafli

--- db/migrations/0004_add_trigger_selesai_pesanan.sql
+++ db/migrations/0004_add_trigger_selesai_pesanan.sql
@@ -4,23 +4,23 @@ DECLARE pelanggan UUID;
 DECLARE pekerja UUID;
 DECLARE biaya DECIMAL;
 BEGIN
-    SELECT PJ.id_pelanggan INTO pelanggan FROM TR_PEMESANAN_STATUS PS
-    JOIN TR_PEMESANAN_JASA PJ ON (PS.id_tr_pemesanan = PJ.id) 
+    SELECT PJ.id_pelanggan INTO pelanggan FROM sijarta.TR_PEMESANAN_STATUS PS
+    JOIN sijarta.TR_PEMESANAN_JASA PJ ON (PS.id_tr_pemesanan = PJ.id) 
     WHERE PS.id_tr_pemesanan = NEW.id_tr_pemesanan;
 
-    SELECT PJ.id_pekerja INTO pekerja FROM TR_PEMESANAN_STATUS PS
-    JOIN TR_PEMESANAN_JASA PJ ON (PS.id_tr_pemesanan = PJ.id) 
+    SELECT PJ.id_pekerja INTO pekerja FROM sijarta.TR_PEMESANAN_STATUS PS
+    JOIN sijarta.TR_PEMESANAN_JASA PJ ON (PS.id_tr_pemesanan = PJ.id) 
     WHERE PS.id_tr_pemesanan = NEW.id_tr_pemesanan;
 
-    SELECT PJ.total_biaya INTO biaya FROM TR_PEMESANAN_STATUS PS
-    JOIN TR_PEMESANAN_JASA PJ ON (PS.id_tr_pemesanan = PJ.id) 
+    SELECT PJ.total_biaya INTO biaya FROM sijarta.TR_PEMESANAN_STATUS PS
+    JOIN sijarta.TR_PEMESANAN_JASA PJ ON (PS.id_tr_pemesanan = PJ.id) 
     WHERE PS.id_tr_pemesanan = NEW.id_tr_pemesanan;
 
     IF NEW.Id_status = 'f33e4567-e89b-12d3-a456-426614174004' THEN
         UPDATE SIJARTA.USER SET saldo_mypay = saldo_mypay + biaya
         WHERE id = pekerja;
 
-        INSERT INTO TR_MYPAY VALUES
+        INSERT INTO sijarta.TR_MYPAY VALUES
         (gen_random_uuid(), pekerja, NOW(), biaya, 'f23e4567-e89b-12d3-a456-426614174005');
     END IF;
     RETURN NEW;
@@ -28,6 +28,6 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trg_selesai_pesanan
-AFTER UPDATE ON TR_PEMESANAN_STATUS
+AFTER UPDATE ON sijarta.TR_PEMESANAN_STATUS
 FOR EACH ROW
 EXECUTE FUNCTION selesai_pesanan(); 
